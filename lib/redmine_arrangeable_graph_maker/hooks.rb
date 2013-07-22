641fe8e61ae7ab2a500801e053c23ca24292910e
Kota Shiratsuka <kota@insaneworks.co.jp>
2013-07-22 09:15:18 +0900

    Add Compatibility for Redmine 2.3
    * add utf8 header
    * add config/routes.rb

--- lib/redmine_arrangeable_graph_maker/hooks.rb
+++ lib/redmine_arrangeable_graph_maker/hooks.rb
@@ -1,6 +1,6 @@
 module RedmineArrangeableGraphMaker
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_issues_sidebar_planning_bottom,
+    render_on :view_projects_show_sidebar_bottom,
               :partial => 'hooks/redmine_arrangeable_graph_maker/view_issues_sidebar_planning_bottom'
   end
 end
